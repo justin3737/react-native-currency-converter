6dc34a0709b0157984814752616f963eb483c84b
Justin Wu <juns3700@gmail.com>
2017-06-28 23:48:06 +0800

    add currencies reducers

--- app/actions/currencies.js
+++ app/actions/currencies.js
@@ -1,7 +1,7 @@
 export const CHANGE_CURRENCY_AMOUNT = 'CHANGE_CURRENCY_AMOUNT';
 export const SWAP_CURRENCY = 'SWAP_CURRENCY';
 
-export const changeCurrencyAmount = (amount) => ({
+export const changeCurrencyAmount = amount => ({
   type: CHANGE_CURRENCY_AMOUNT,
   amount: parseFloat(amount),
 });
