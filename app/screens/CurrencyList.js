04b728675b6a1bc98c140daa46bda56f492bf827
Justin Wu <juns3700@gmail.com>
2017-07-02 00:02:18 +0800

    currencies list action

--- app/screens/CurrencyList.js
+++ app/screens/CurrencyList.js
@@ -1,39 +1,49 @@
 import React, { Component, PropTypes } from 'react';
-import { FlatList, View, StatusBar } from 'react-native';
+import { FlatList, StatusBar, View } from 'react-native';
+import { connect } from 'react-redux';
 
-import { ListItem, Separator } from '../components/List'
+import { ListItem, Separator } from '../components/List';
 import currencies from '../data/currencies';
+import { changeBaseCurrency, changeQuoteCurrency } from '../actions/currencies';
 
-const TEMP_SELECTED_CURRENCY = 'CAD';
+const TEMP_CURRENT_CURRENCY = 'CAD';
 
 class CurrencyList extends Component {
-    static proptypes = {
-        navigation: PropTypes.object
+  static propTypes = {
+    navigation: PropTypes.object,
+    dispatch: PropTypes.func,
+  };
+
+  handlePress = (currency) => {
+    const { type } = this.props.navigation.state.params;
+    if (type === 'base') {
+      this.props.dispatch(changeBaseCurrency(currency));
+    } else if (type === 'quote') {
+      this.props.dispatch(changeQuoteCurrency(currency));
     }
 
-    _handlePress = () => {
-        this.props.navigation.goBack(null);
-    }
+    this.props.navigation.goBack(null);
+  };
 
-    render () {
-        return (
-             <View style={{ flex: 1 }}>
-                <StatusBar barStyle="default" translucent={false}/>
-                <FlatList
-                    data={currencies}
-                    renderItem={({ item }) => 
-                        <ListItem
-                            text={item}
-                            selected={item === TEMP_SELECTED_CURRENCY}
-                            onPress={this._handlePress}
-                        />
-                    }
-                    keyExtractor={item => item}
-                    ItemSeparatorComponent={Separator}
-                />
-            </View>
-        );
-    }
-};
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <StatusBar translucent={false} barStyle="default" />
+        <FlatList
+          data={currencies}
+          renderItem={({ item }) => (
+            <ListItem
+              text={item}
+              selected={item === TEMP_CURRENT_CURRENCY}
+              onPress={() => this.handlePress(item)}
+            />
+          )}
+          keyExtractor={item => item}
+          ItemSeparatorComponent={Separator}
+        />
+      </View>
+    );
+  }
+}
 
-export default CurrencyList;
+export default connect()(CurrencyList);
