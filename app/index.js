96984eab1f56f7dc510c97cd638072a3a9f9c8d0
Justin Wu <juns3700@gmail.com>
2017-07-01 17:42:42 +0800

    add Home loading...

--- app/index.js
+++ app/index.js
@@ -12,18 +12,17 @@ EStyleSheet.build({
   $primaryGreen: '#00BD9D',
   $primaryPurple: '#9E768F',
 
-  $border: '#E2E2E2',
   $white: '#FFFFFF',
-  $inputText: '#797979',
   $lightGray: '#F0F0F0',
+  $border: '#E2E2E2',
+  $inputText: '#797979',
   $darkText: '#343434',
-  // outline:1,
 });
 
 export default () => (
   <Provider store={store}>
     <AlertProvider>
-      <Navigator onNavigationStateChange={null}/>
+      <Navigator onNavigationStateChange={null} />
     </AlertProvider>
   </Provider>
 );
